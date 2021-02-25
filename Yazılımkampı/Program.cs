407abda5b4fc08824415f90b2ec819d8aac5227a
18853 <18853@of365.online>
2021-02-25 16:36:36 +0300

    Add project files.

---
+++ Yazılımkampı/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yazılımkampı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
